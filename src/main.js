2d7f7073aca977cf0ba8f477139bfe635ea5c05a
Yujia Ding <yujia.ding@polymtl.ca>
2021-03-08 20:30:48 -0500

    links done

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faInstagram, faPaypal } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faInstagram,faPaypal)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
