90e39d3912cbd910e4a64bb749cf4054642a9f64
Yujia Ding <yujia.ding@polymtl.ca>
2021-04-28 20:12:35 -0400

    changed intro paragraph

--- src/views/home.vue
+++ src/views/home.vue
@@ -3,8 +3,8 @@
     <h1>(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧</h1>
     <div>
       <img class="intro" src="../assets/headshot.png">
-          <p class="intro" >Henlo everybun, Bemon here! I've been drawing for 23 years, ever since I was 3 years old. My favorites are portraits and scenery. I can do traditional or digital.
-          For the moment, all my commissions are digital only. My prices and rules are under the commission tag, I'd love to share my talents with you. </p>
+          <p class="intro" >Henlo everybun, Bemon here! I've been drawing for 23 years, ever since I was 3 years old. My favorites are portraits and scenery, which I can do with both traditional and digital.
+          For the moment, all my commissions are digital only. My prices and rules are under the commission tag, I'd love to share my talents. Looking forward to drawing something for you~</p>
     </div>
   </div>
 </template>
