7dd249efa822afacdf67bcaf66eba4dd5ef11ac4
Yujia Ding <yujia.ding@polymtl.ca>
2021-04-29 21:59:00 -0400

    added assets and started commissions page

--- src/views/commission.vue
+++ src/views/commission.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     <h1>Commission rules</h1>
+    <img class="commissions" src="../assets/transparent_lemon.png">
+    <p class="rules">
+    -For personal use only. <br>
+    -Payment through PayPal. <br>
+    -2-3 weeks turnaround time. <br>
+    -25% off extra characters. <br>
+
+    I will draw: PG 13, couples, animals, fanart, OCs, portraits. <br>
+    I reserve the right to refuse requests!
+
+    Reach me on any of the mentionned socials :3</p>
 
   </div>
 </template>
@@ -12,4 +23,21 @@ export default {
 
 }
 </script>
+
+<style>
+.commissions{
+  width: 200px;
+  height: 200px;
+  margin-right: 50px;
+}
+.rules{
+   display: inline-block;
+   vertical-align: top;
+   text-align: left;
+   width: 300px;
+   height: 300px;
+   padding: 5px;
+}
+
+</style>
 
